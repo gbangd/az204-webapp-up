471c175f96ac0d401460cabc972e7a954295ff1f
gbangd <gbanguera@gmail.com>
2022-12-06 20:41:28 -0500

    Update folder structure

---
+++ Logic/EllipseDimensions.cs
@@ -0,0 +1,10 @@
+namespace ImageCreator.Logic
+{
+    public struct EllipseDimensions
+    {
+        public int X {get;set;}
+        public int Y {get;set;}
+        public int Widht {get;set;}
+        public int Height {get;set;}
+    }
+}
